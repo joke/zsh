e5bf8a288351df1b6ca1e8b806371bb21840b8f4
Joke de Buhr <joke@seiken.de>
2020-09-18 14:24:49 +0200

    fix update interval

--- autoupdate-antibody/autoupdate-antibody.plugin.zsh
+++ autoupdate-antibody/autoupdate-antibody.plugin.zsh
@@ -5,7 +5,7 @@ function _autoupdate_antibody() {
     local home=$ANTIBODY_HOME
     local days=${AUTOUPDATE_ANTIBODY_DAYS:=7}
     [[ -z $home ]] && home="$(antibody home)"
-    if [[ -n $home/*/.git/FETCH_HEAD(#qY1N-md+${days}) ]]; then
+    if [[ -n $home/*/.git/FETCH_HEAD(#qY1N-mh+$((days*24))) ]]; then
       antibody update
       touch $home 
       [[ $AUTOUPDATE_ANTIBODY_RESTART = true && -n $home/*/.git/objects(#qN-mh-$((days*24))) ]] && exec "$@"
